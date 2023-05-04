b82e8e3bc9d7f13ef0d6acc2dfbcfb166d81150a
GreenTim123 <132196744+GreenTim123@users.noreply.github.com>
2023-05-04 12:25:49 +0200

    Update game.py
    eeee

--- game.py
+++ game.py
@@ -1,14 +1,1 @@
-from pywebio import *
-from pywebio.input import *
-from pywebio.output import *
-from pywebio.platform.flask import start_server
 
-def chat():
-    put_text("Welcome to the chat!")
-    name = input("What's your name?", type=TEXT)
-    while True:
-        message = input("Enter your message:", type=TEXT)
-        put_text(name + ": " + message)
-
-if __name__ == '__main__':
-    start_server(chat, port=8080)
